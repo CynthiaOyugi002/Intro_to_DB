d51036cd0ec072162eec95ab358ccaa6485d7291
Cynthia Oyugi <cynthiaoyugi370@gmail.com>
2025-10-25 18:13:34 +0300

    Add MSQLServer.py to create alx_book_store database

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,3 +1,4 @@
+
 """
 This script creates the database 'alx_book_store' in the MySQL server.
 If the database already exists, the script will not fail.
@@ -16,17 +17,17 @@ try:
 
     if connection.is_connected():
         cursor = connection.cursor()
+    
         cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
         print("Database 'alx_book_store' created successfully!")
 
 except Error as e:
+    
     print(f"Error while connecting to MySQL: {e}")
 
 finally:
-    try:
-        if connection.is_connected():
-            cursor.close()
-            connection.close()
-            print("MySQL connection closed.")
-    except NameError:
-        print("Connection was never established.")
+    
+    if 'connection' in locals() and connection.is_connected():
+        cursor.close()
+        connection.close()
+        print("MySQL connection closed.")
