74f256e51eab5d9e29cb6ee7604318d79e2f1141
Cynthia Oyugi <cynthiaoyugi370@gmail.com>
2025-10-25 18:32:21 +0300

    Add task_4.sql to show full description of books table without DESCRIBE or ANALYZE

--- task_4.sql
+++ task_4.sql
@@ -1,5 +1,7 @@
-
+-- This script prints the full description of the table 'books'
+-- from the database 'alx_book_store' in your MySQL server.
+-- It does not use DESCRIBE or ANALYZE statements.
 
 USE alx_book_store;
 
-SHOW CREATE TABLE books;
+SHOW COLUMNS FROM books;
